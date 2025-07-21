afa4d3485bb591b3830b3172917c021f8b486152
yuikimura <edikanorusiruru@outlook.jp>
2025-07-21 16:51:28 +0900

    プッシュ4

--- Assets/Scripts/RewardDetail.cs
+++ Assets/Scripts/RewardDetail.cs
@@ -1,40 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// 褒賞一覧の1つひとつ（アイコン）の表示やボタン押下時の動作を制御するクラス
+/// </summary>
 public class RewardDetail : MonoBehaviour
 {
+    // 褒賞の画像を表示するUIコンポーネント
     [SerializeField]
     private Image imgReward;
 
+    // このオブジェクトに紐づく褒賞データ
     [SerializeField]
     private RewardData rewardData;
 
+    // 褒賞アイコンをタップしたときのボタン
     [SerializeField]
     private Button btnRewardDetail;
 
+    // 表示に使う AlbumPopUp を保持（ポップアップUIとのやりとり用）
     private AlbumPopUp albumPopUp;
 
     /// <summary>
-    /// RewardDetail の設定
+    /// 褒賞アイコンの見た目や動作を設定する
     /// </summary>
-    /// <param name="rewardData"></param>
-    /// <param name="albumPopUp"></param>
+    /// <param name="rewardData">表示する褒賞のデータ</param>
+    /// <param name="albumPopUp">画像表示用の親ポップアップ</param>
     public void SetUpRewardDetail(RewardData rewardData, AlbumPopUp albumPopUp)
     {
+        // 褒賞データと親ポップアップを記録
         this.rewardData = rewardData;
         this.albumPopUp = albumPopUp;
 
+        // 褒賞アイコンの画像を設定
         imgReward.sprite = this.rewardData.spriteReward;
 
+        // ボタン押下時のイベントを登録（クリックで詳細表示）
         btnRewardDetail.onClick.AddListener(OnClickRewardDetail);
     }
 
     /// <summary>
-    /// btnRewardDetail をおした際の処理
+    /// 褒賞アイコンをクリックしたときの処理（大きく表示）
     /// </summary>
     public void OnClickRewardDetail()
     {
-        albumPopUp.DisplayReward(rewardData.spriteReward);      // 　<=　このメソッドが AlbumPopUp スクリプト側にないため、エラーが出ます。準備後にコメントアウトを解除してください
+        // ポップアップに画像を渡して表示
+        albumPopUp.DisplayReward(rewardData.spriteReward);
     }
 }
